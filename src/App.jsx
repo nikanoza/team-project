47463440bf696196d07d9d2fef7263e8bc8c3493
Nika Nozadze <nikanozadze1010@gmail.com>
2024-03-26 10:47:45 +0400

    feat: add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import { AddBlog, Blog, Home } from "./pages";
+
 function App() {
-  return <main className="w-full h-full"></main>;
+  return (
+    <main className="w-full h-full">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/add-blog" element={<AddBlog />} />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
